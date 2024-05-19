589370037d2f5548cf725eaed940264422a10273
wjdaudtlr <kim__aaa@naver.com>
2024-05-20 01:40:48 +0900

    added Servo_Read() function in servo.h
    implemented command parser
    
    implemented command handler
    
    implemented some of excute function(have to implement more)

--- Inharo_fsw/Core/Inc/module/servo/servo.c
+++ Inharo_fsw/Core/Inc/module/servo/servo.c
@@ -44,3 +44,26 @@ HAL_StatusTypeDef Servo_Write(Servo_HandleTypeDef *servo, int deg){
 	}
 	return HAL_OK;
 }
+int Servo_Read(Servo_HandleTypeDef *servo){
+	int us, deg;
+	switch (servo->channel){
+		case TIM_CHANNEL_1:
+			us = servo->htim->Instance->CCR1;
+			break;
+		case TIM_CHANNEL_2:
+			us = servo->htim->Instance->CCR2;
+			break;
+		case TIM_CHANNEL_3:
+			us = servo->htim->Instance->CCR3;
+			break;
+		case TIM_CHANNEL_4:
+			us = servo->htim->Instance->CCR4;
+			break;
+		default:
+			return -1;
+		}
+	deg = _SERVO_US2DEG(us);
+	deg = _SERVO_SAT(deg, SERVO_DEG_MIN, SERVO_DEG_MAX);
+	return deg;
+}
+
